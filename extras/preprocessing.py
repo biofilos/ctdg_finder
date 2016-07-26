e9622019fab874c5f346497e03c35dedf88c01ec
alekos <juanfortizq@gmail.com>
2016-07-26 13:52:14 -0500

    prep

--- extras/preprocessing.py
+++ extras/preprocessing.py
@@ -8,7 +8,7 @@ from concurrent import futures
 Entrez.email = 'juan.f.ortiz@vanderbilt.edu'
 
 GENOME_DB = sys.argv[1]
-CPUS = 6
+CPUS = 8
 
 
 def parse_gb():
@@ -33,7 +33,10 @@ def parse_gb():
                 end_feat = feat.location.end.position
                 strand_feat = feat.location.strand
                 acc_feat = feat.qualifiers['protein_id'][0]
-                symbol_feat = feat.qualifiers['gene'][0]
+                try:
+                    symbol_feat = feat.qualifiers['gene'][0]
+                except KeyError:
+                    symbol_feat = acc_feat
                 # Try/except was here
                 if 'product' in feat.qualifiers.keys():
                     name = feat.qualifiers['product'][0]
@@ -158,7 +161,10 @@ def dict_seq(fasta):
     for line in open(fasta):
         line = line.strip('\n')
         if line.startswith('>'):
-            name = line.strip('>').split('|')[3]
+            if not "|" in line:
+                name = line.strip('>').split(' ')[0]
+            else:
+                name = line.strip('>').split('|')[3]
             seq_dict[name] = ''
         else:
             seq_dict[name] += line
@@ -185,9 +191,9 @@ def seq_column(accession):
 # Parse Genbank
 if not os.path.exists('genes_cds.csv'):
     out_data = parse_gb()
-    out_data =  out_data.loc[~(out_data['name'].map(lambda x: 'isoform X' in x))]
+    out_data = out_data.loc[~(out_data['name'].map(lambda x: 'isoform X' in x))]
     # Save full annotation
-    out_data['species'] = out_data['species'].map(lambda x: x.replace(' ','_')
+    out_data['species'] = out_data['species'].map(lambda x: x.replace(' ','_'))
     out_data.to_csv('genes_cds.csv')
     # Remove some columns
     mini = out_data.loc[:, ['species', 'chromosome', 'symbol', 'start', 'end', 'length', 'strand']]
@@ -262,7 +268,7 @@ with futures.ProcessPoolExecutor(CPUS) as pool:
 
 assembly_table = pd.DataFrame(list(assembly_list), columns=['sp', 'chromosome', 'taxid',
                                                       'GI', 'chr_acc', 'Assembly', 'length'])
-assembly_table['sp'] = assembly_table['sp'].map(lambda x: x.replace(' ','_')
+assembly_table['sp'] = assembly_table['sp'].map(lambda x: x.replace(' ', '_'))
 
 assembly_table.to_csv('chromosomes.csv')
 print("Assembly file, generated")
