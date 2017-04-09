d50bad472ba5ed319884d3fe95875edf40a86841
Juan Felipe Ortiz <juanfortizq@gmail.com>
2017-04-08 19:05:32 -0500

    generate empty dictionary

--- extras/hits_dictionary.jl
+++ extras/hits_dictionary.jl
@@ -17,17 +17,17 @@ end
 
 genes_df = readtable(genes_file)
 
-genes_annotated = join(genes_df, gene_pfam, on=:acc)
+genes_annotated = join(genes_df, gene_pfam, on=:acc, kind=:left)
 
 for chrom_table = groupby(genes_annotated, [:species, :chromosome])
   species = chrom_table[:species][1]
   chrom = chrom_table[:chromosome][1]
   chrom_file = format("{}/hmmers/{}_{}.json", db, species, chrom)
-  println(chrom_file)
   chrom_dict = Dict()
   for gene in unique(chrom_table[:acc])
     gene_df = chrom_table[chrom_table[:acc].==gene, :]
-    pfams_accs = chrom_table[findin(chrom_table[:pfam],gene_df[:pfam]),:]
+    gene_df = gene_df[~(isna(gene_df[:pfam])),:]
+    pfams_accs = chrom_table[(findin(chrom_table[:pfam],gene_df[:pfam])),:]
     chrom_dict[gene] = unique(pfams_accs[:acc])
   end
   dict_str = JSON.json(chrom_dict)
