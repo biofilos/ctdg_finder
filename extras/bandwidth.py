d0b19e7e0b39654eb1e0f500f7d0cc5360cde833
Juan Felipe Ortiz <juanfortizq@gmail.com>
2019-02-13 13:13:14 -0600

    generate json

--- extras/bandwidth.py
+++ extras/bandwidth.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from sys import argv
 from concurrent import futures
-
+import json
 """
 Given a database directory, calculate the bandwidth parameter for
 MeanShift and include it in the chromosomes.csv file
 """
 
 db_dir = argv[1]
-
+CPU = int(argv[2])
 
 def intergenic(c_table):
     # If there is only one gene in the chromosome
@@ -40,11 +40,16 @@ def get_bw(ch_ix):
     chr_df = chromosomes.loc[ch_ix]
     sp = chr_df["species"]
     chrom = chr_df["chromosome"]
-    length = int(chr_df["length"])
+    try:
+        length = int(chr_df["length"])
+    except ValueError:
+        print(chr_df)
+        assert False, "e"
     sp_ch_genes = genes.loc[(genes["species"] == sp) &
                             (genes["chromosome"] == chrom)]
     bw = intergenic(sp_ch_genes)
     str_out = "{},{},{},{}\n".format(sp, chrom, length, bw)
+    print(str_out)
     return str_out
 
 
@@ -53,12 +58,24 @@ chromosomes = pd.read_csv(db_dir + "/pre_chromosomes.csv")
 genes.sort_values(["species", "chromosome", "start"], inplace=True)
 
 # Calculate bandwidth for each chromosome
-with futures.ProcessPoolExecutor(8) as pool:
+with futures.ProcessPoolExecutor(CPU) as pool:
     bw_map = pool.map(get_bw,  chromosomes.index)
 
-# Set file
+# Set files
 chrom_file = open(db_dir + "/chromosomes.csv", "w")
 chrom_file.write("species,chromosome,length,bandwidth\n")
-for chrom in bw_map:
-    chrom_file.write(chrom)
+dict_out = open(db_dir + "/chromosomes.json", "w")
+chrom_d = dict()
+for chrom_data in bw_map:
+    sp, chrom, length, bw = chrom_data.strip().split(",")
+    # Save data as dictionary
+    if sp not in chrom_d:
+        chrom_d[sp] = {chrom:[int(length), float(bw)]}
+    else:
+        chrom_d[sp][chrom] = [int(length), float(bw)]
+    chrom_file.write(chrom_data)
 chrom_file.close()
+
+# Write dictionary as json
+dict_out.write(json.dumps(chrom_d))
+dict_out.close()
