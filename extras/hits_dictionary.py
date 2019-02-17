d9d489efc0dd5ae60685e93dc1142e37c7bf3190
Juan Felipe Ortiz <juanfortizq@gmail.com>
2019-02-17 11:56:26 -0600

    remove conditional that won't generate files if they exist

--- extras/hits_dictionary.py
+++ extras/hits_dictionary.py
@@ -47,32 +47,30 @@ def rec2dict_sp(sp):
     """
     sp_chrom: list, where the first element is species
     """
-# for sp, chrom in set(genes.set_index(["species", "chromosome"]).index.values):
     sp_json_file = db + "/sp_hmmers/{}.json".format(sp)
-    if not os.path.exists(sp_json_file):
-        # Initialize dictionary of Pfam hits
-        pfam_in_chrom = {}
-        for chrom in genes.loc[genes["species"] == sp, "chromosome"].unique():
-            print("Processing {} ({})".format(sp, chrom))
-            # Set of genes in the chromosome
-            chrom_genes = set(genes.loc[(genes["species"] == sp) &
-                                        (genes["chromosome"] == chrom), "acc"].values)
-            # Go through each gene
-            for gene in chrom_genes:
-                same_pfams = []
-                for pfam in pfams:
-                    # Set of genes for each pfam accession
-                    pfam_set = set(pfams[pfam])
-                    # If the reference gene has that domain
-                    if gene in pfam_set:
-                        # Add all the genes in the chromosome that have that domain
-                        same_pfams += list(chrom_genes.intersection(pfam_set))
-                # Add all those genes as a non-redundant list
-                pfam_in_chrom[gene] = list(set(same_pfams))
-        # Save to file
-        chrom_out = open(sp_json_file, "w")
-        chrom_out.write(json.dumps(pfam_in_chrom))
-        chrom_out.close()
+    # Initialize dictionary of Pfam hits
+    pfam_in_chrom = {}
+    for chrom in genes.loc[genes["species"] == sp, "chromosome"].unique():
+        print("Processing {} ({})".format(sp, chrom))
+        # Set of genes in the chromosome
+        chrom_genes = set(genes.loc[(genes["species"] == sp) &
+                                    (genes["chromosome"] == chrom), "acc"].values)
+        # Go through each gene
+        for gene in chrom_genes:
+            same_pfams = []
+            for pfam in pfams:
+                # Set of genes for each pfam accession
+                pfam_set = set(pfams[pfam])
+                # If the reference gene has that domain
+                if gene in pfam_set:
+                    # Add all the genes in the chromosome that have that domain
+                    same_pfams += list(chrom_genes.intersection(pfam_set))
+            # Add all those genes as a non-redundant list
+            pfam_in_chrom[gene] = list(set(same_pfams))
+    # Save to file
+    chrom_out = open(sp_json_file, "w")
+    chrom_out.write(json.dumps(pfam_in_chrom))
+    chrom_out.close()
     print("*{} done".format(sp))
 
 verbose(2)
