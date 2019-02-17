b00253ea9bccf46bc02c80fabb267cc5bf316860
Juan Felipe Ortiz <juanfortizq@gmail.com>
2019-02-17 11:57:14 -0600

    explicitly invokes python3

--- extras/run_hmmer.sh
+++ extras/run_hmmer.sh
@@ -12,7 +12,7 @@ done
 
 # Calculate bandwidth parameter
 echo Calculating bandwidth
-python $script_dir/bandwidth.py $db_dir $cpus
+python3 $script_dir/bandwidth.py $db_dir $cpus
 # Generate hmmer database
 hmmpress $db_dir/panther/panther.hmm
 # Run HMMsearch
@@ -20,14 +20,14 @@ echo Running HMMeR
 hmmsearch --cpu $cpus --noali -E 0.0001 -o $db_dir/hmmer/all.hmmer --tblout $db_dir/hmmer/all.tbl --domtblout $db_dir/hmmer/all.dom $db_dir/panther/panther.hmm $db_dir/seqs_hmmer.fa
 # Parse hmmer output
 echo Parsing HMMeR output
-python $script_dir/parse_big_hmmer_strict.py $db_dir/hmmer/all.dom $db_dir/hmmer/pfam.json $db_dir/hmmer/hmmer_filtered.csv
+python3 $script_dir/parse_big_hmmer_strict.py $db_dir/hmmer/all.dom $db_dir/hmmer/pfam.json $db_dir/hmmer/hmmer_filtered.csv
 # Generate chromosome-specific JSON files
 echo Generating dictionaries
-python $script_dir/hits_dictionary.py $db_dir $cpus
+python3 $script_dir/hits_dictionary.py $db_dir $cpus
 # Generate homology graphs
 echo Generating homology graphs
-python $script_dir/domains_net.py $db_dir/hmmer/pfam.json $db_dir/genes_parsed.csv $db_dir/graphs $cpus
+python3 $script_dir/domains_net.py $db_dir/hmmer/pfam.json $db_dir/genes_parsed.csv $db_dir/graphs $cpus
 # Generate homology matrices
 echo Generating homology matrices
-python $script_dir/parse_matrices.py $db_dir/graphs $db_dir/matrices
+python3 $script_dir/parse_matrices.py $db_dir/graphs $db_dir/matrices
 
